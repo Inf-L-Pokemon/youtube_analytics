27cc73d410b3bc8843b1c69231dbe306bb5f2f68
Sergey Korchmin <sergey.korchmin@yandex.ru>
2023-12-29 23:19:43 +0300

    some little change after formatter "black"

--- src/channel.py
+++ src/channel.py
@@ -9,8 +9,8 @@ class Channel:
     """Класс для ютуб-канала"""
 
     load_dotenv()
-    api_key: str = os.getenv("YT_API_KEY")
-    youtube = build('youtube', 'v3', developerKey=api_key)
+    api_key: str | None = os.getenv("YT_API_KEY")
+    youtube = build("youtube", "v3", developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -18,5 +18,5 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = Channel.youtube.channels().list(id=self.channel_id, part="snippet,statistics").execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
